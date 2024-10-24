ed7542bfd19fb33d3065383f0117f29dc5561d3c
lookplarcnk <83052144+lookplarcnk@users.noreply.github.com>
2024-10-25 03:03:12 +0700

    Add files via upload

---
+++ api/README.md
@@ -0,0 +1,21 @@
+## To run the api use this command
+
+  
+- `npm install --force`: install npm.
+- `npm start`: Start the project.
+
+**Example:**
+
+  
+```command Prompt
+
+npm install -- force
+npm start
+
+```
+
+  
+
+**Result:**
+
+![Pasted image 20241024211420](https://github.com/user-attachments/assets/c9fbe39c-aa1a-4e92-88c4-2eb985a8854d)
---
+++ api/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3080;
+
+// place holder for the data
+const users = [
+  {
+    firstName: "Kaweewat",
+    lastName: "Kansupattanakul",
+    email: "kaweeewat_ka65@live.rmutl.ac.th",
+    position: "developer"
+  },
+  {
+    firstName: "Ratchaneekorn",
+    lastName: "Chuadee",
+    email: "Ratchaneekorn_ra65@live.rmutl.ac.th",
+    position: "system analyst"
+  },
+  {
+    firstName: "aphinan",
+    lastName: "hongpong",
+    email: "aphinan_ap65@live.rmutl.ac.th",
+    position: "tester"
+  }
+];
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../my-app/build')));
+
+app.get('/api/users', (req, res) => {
+  console.log('api/users called!')
+  res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  console.log('Adding user:::::', user);
+  users.push(user);
+  res.json("user addedd");
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
